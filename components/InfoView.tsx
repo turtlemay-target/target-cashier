4ae96b39ab65259e3a1ac77521d3656643ae5bef
Turtlemay <am@trtl.in>
2020-10-12 18:04:52 -0400

    Updated info.

--- components/InfoView.tsx
+++ components/InfoView.tsx
@@ -9,8 +9,8 @@ export const InfoView = () => (
 				<p>This app assists Target cashiers in scanning produce, looking up item codes, and performing calculations.</p>
 				<h2>✨ Features</h2>
 				<ul>
-					<li>Enter an item's name in the query box to display a list of barcodes found in the database.</li>
-					<li>Enter a UPC or PLU code to generate a barcode.</li>
+					<li>Enter search text into the query box to display a list of items found in the database.</li>
+					<li>Enter a UPC, PLU, or DPCI code to generate a data bar.</li>
 					<li>Multiple queries can be chained with semicolon (default <Link to="/prefs">user setting</Link>).</li>
 					<li>Mathematical expressions entered in the query box will be automatically evaluated.</li>
 					<li>Enter a two digit number to calculate round-up amount.</li>
