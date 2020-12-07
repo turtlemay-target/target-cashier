019aae5a4625739cd79537f758131ae4cecfd60a
Turtlemay <am@trtl.in>
2020-12-06 19:23:53 -0500

    Changed link to http.

--- components/InfoView.tsx
+++ components/InfoView.tsx
@@ -27,7 +27,7 @@ export const InfoView = () => (
 			<section>
 				<p><span>Software version <strong>{packageJson.version}</strong>.</span></p>
 				<p>We use the latest browser APIs so ensure your OS, browser and/or WebView is up to date. An Android device with peripheral keyboard is recommended.</p>
-				<p>Created and maintained by <a target="_blank" href="https://turtlemay.us">Turtlemay</a>.</p>
+				<p>Created and maintained by <a target="_blank" href="http://turtlemay.us">Turtlemay</a>.</p>
 			</section>
 		</div>
 	</div>
