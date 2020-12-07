d2919db4b28d8e2797c2764cee70ea469943a05b
Turtlemay <am@trtl.in>
2020-12-06 19:48:23 -0500

    Using default import for package.json
    (cherry picked from commit 6c7a2cdfe34f2e225d7747d07e6108e97b20e8d8)

--- components/InfoView.tsx
+++ components/InfoView.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import * as packageJson from '../package.json'
+import packageJson from '../package.json'
 import { Link } from 'react-router-dom'
 
 export const InfoView = () => (
