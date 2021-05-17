a0abb8ed5fd824addba043f2f870062e4edc95e7
Turtlemay <am@trtl.in>
2021-05-17 00:34:20 -0400

    Changed text.

--- components/MainView.tsx
+++ components/MainView.tsx
@@ -225,7 +225,7 @@ export const MainView = (props: {
 				<DelayedTextInput className={c('mainView__mainInput', { 'mainView__mainInput--numType': useNumInput })}
 					type={useNumInput ? 'number' : 'text'}
 					elemRef={inputElemRef}
-					placeholder={useNumInput ? "Enter UPC, PLU, or DPCI" : "Enter query"}
+					placeholder={useNumInput ? "Enter UPC or PLU" : "Enter query"}
 					committedValue={query}
 					onStartInput={() => setThrobber(true)}
 					onStopInput={() => setThrobber(false)}
